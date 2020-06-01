b4ce191592abeeece29f34eb5c8585ab924138de
Amin Ahrabian <aahrabian@gmail.com>
2020-06-01 19:29:27 +0200

    Connect Android to CPP

--- android/app/src/main/java/com/reactcppbridge/nativemodules/StringMessageModule.kt
+++ android/app/src/main/java/com/reactcppbridge/nativemodules/StringMessageModule.kt
@@ -4,14 +4,27 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.reactcppbridge.StringMessage
 
 class StringMessageModule(reactContext: ReactApplicationContext) :
         ReactContextBaseJavaModule(reactContext) {
+
+    companion object {
+
+        init {
+            System.loadLibrary("stringmessage")
+        }
+    }
+
+    val cppApi = StringMessage.create()
 
     override fun getName() = "StringMessage"
 
     @ReactMethod
     fun newMessage(promise: Promise) {
-        promise.resolve("Cazzo!")
+
+        val message = cppApi.stringMessage
+
+        promise.resolve(message)
     }
 }
